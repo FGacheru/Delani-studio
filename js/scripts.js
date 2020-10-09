4de47b0f1553d82d9a9fb2071e2142c8fd57abc8
FGacheru <francisgacheru2001@gmail.com>
2020-10-09 16:08:53 +0300

    made the submit button responsive

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,19 @@ $("#img3").click(function(){
  $("#p3").toggle("slow");   
 });
 });
+
+
+$(Document).ready(function(){
+    $("#img4").mouseover(function(){
+    $("p4").css("opacity:1")    
+    })
+    $("#img4").mouseout(function(){
+        $("p4").css("opacity:1")    
+        })
+})
+
+$("button").click(function () {
+    var client = document.getElementById('details').value;
+    alert(' Dear ' + client + ' we have received your message successfully. ' + ' Feel free to reach out to us at anytime');
+
+  });
