511dbc5c2a51e5db0f781d8429e193d08069e9be
FGacheru <francisgacheru2001@gmail.com>
2020-10-10 12:19:50 +0300

    added hover to jquery

--- js/scripts.js
+++ js/scripts.js
@@ -11,17 +11,38 @@ $("#img3").click(function(){
 });
 
 
-$(Document).ready(function(){
-    $("#img4").mouseover(function(){
-    $("p4").css("opacity:1")    
-    })
-    $("#img4").mouseout(function(){
-        $("p4").css("opacity:1")    
-        })
-})
+// $(Document).ready(function(){
+//     $("#img4").mouseover(function(){
+//     $("p4").css("opacity:1")    
+//     })
+//     $("#img4").mouseout(function(){
+//         $("p4").css("opacity:1")    
+//         })
+// })
+
+// $(".card").hover(function () {
+//     $(this).children(".card-1").fadeToggle(1000, "linear");
+//   });
+
 
 $("button").click(function () {
     var client = document.getElementById('details').value;
     alert(' Dear ' + client + ' we have received your message successfully. ' + ' Feel free to reach out to us at anytime');
 
   });
+
+  
+  
+$(document).ready(function () {
+  $(".p4").hide();
+  $(".p5").hide();
+  $(".p6").hide();
+  $(".p7").hide();
+  $(".img4").hover(function () {
+      $(this).find(".p4").toggle();
+      $(this).find(".p5").toggle();
+      $(this).find(".p6").toggle();
+      $(this).find(".p7").toggle();
+  });
+ 
+});
